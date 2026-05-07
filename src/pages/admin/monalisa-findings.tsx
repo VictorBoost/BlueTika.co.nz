b4530fdee235e402dba1969bccc718d947eee51c
Victorboost <vimonwi@gmail.com>
2026-05-07 12:44:37 +1200

    monalisa

--- src/pages/admin/monalisa-findings.tsx
+++ src/pages/admin/monalisa-findings.tsx
@@ -26,6 +26,7 @@ export default function MonaLisaFindings() {
 
   useEffect(() => {
     fetchFindings();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filter]);
 
   const fetchFindings = async () => {
